c47fdee37124e8b9867a38ce280dcccb283ac2d0
Joan-Acevedo <joan.acevedo@mail.escuelaing.edu.co>
2025-02-17 17:45:17 -0500

    First commit

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -1,11 +1,14 @@
 package edu.eci.arsw.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class GrammarChecker {
 
+	@Autowired
+	@Qualifier("englishSpellChecker")
 	SpellChecker sc;
 
 	String x;
@@ -23,8 +26,9 @@ public class GrammarChecker {
 	public String check(String text){
 		
 		StringBuffer sb=new StringBuffer();
-		sb.append("Spell checking output:"+sc.checkSpell(text));
-		sb.append("Plagiarism checking output: Not available yet");
+		sb.append("Spell checking output:\n"+sc.checkSpell(text)+ "\n");
+		sb.append("_ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _\n");
+		sb.append("Plagiarism checking output: Not available yet ");
 		
 		
 		return sb.toString();
